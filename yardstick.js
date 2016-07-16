f27f445e06b24b97e2848e3513cd39fc535b8112
wilburrr90 <wilburrr90@gmail.com>
2016-07-16 10:19:18 -0400

    added the 'add/move/delte marker' functionality

--- yardstick.js
+++ yardstick.js
@@ -5,47 +5,78 @@ L.tileLayer(
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
     maxZoom: 18,
     }).addTo(map);
-    
-// Put a draggable marker on the map
-var marker = new L.marker([40.740693, -74.004536], {draggable:'true'});
-map.addLayer(marker);
 
+var myMap = document.getElementById("map");
 var markers = new Array();
+var spaceDown = false;
+var markerClick = false;
 
 window.addEventListener("keydown",keyDown);
 window.addEventListener("keyup",keyUp);
-//window.addEventListener("mousedown",mouseDown);
-
-var myMap = document.getElementById("map");
 myMap.addEventListener("mousedown",mouseDown);
 
-var spaceDown = false;
 function keyDown(e){
     if(e.keyCode==32 && spaceDown== false ){
-        //alert("hi");
         myMap.style.cursor="crosshair";
-        console.log("hi");
+        //console.log("hi");
         spaceDown=true;
     }
 }
 
 function keyUp(e){
     if(e.keyCode==32 && spaceDown==true ){
-        //alert("hi");
         myMap.style.cursor="pointer";
-        console.log("hey");
+        //console.log("hey");
         spaceDown=false;
     }
 }
 
 function mouseDown(e){
-    console.log("yo");
-    console.log(e.clientX);
-    console.log(e.clientY);
-    var newMark;
-    if(spaceDown){
-        console.log(map.mouseEventToLayerPoint(e));
-        newMark = new L.marker(map.mouseEventToLatLng(e),{draggable:true});
-        map.addLayer(newMark);
+    var i;
+    if(spaceDown  && markerClick==false){
+        markers.push( new L.marker(map.mouseEventToLatLng(e),
+                                    {draggable:true}) );
+        i = markers.length-1;
+        map.addLayer( markers[i]);
+        markers[i].addEventListener("dragend",drawLines,false);
+        addMoveFunction(markers[i],i);
+        drawLines();
+    } else {
+        markerClick=false;
     }
 }
+
+function onDragEnd(){
+    alert("draggg");
+}
+
+function addMoveFunction(thisMarker,thisIndex){
+    thisMarker.addEventListener("mousedown",function(e){
+        if(spaceDown){
+            map.removeLayer(thisMarker);
+            markers.splice(markers.indexOf(thisMarker),1);
+            console.log( markers.indexOf(thisMarker) );
+            console.log(markers);
+            drawLines();
+            markerClick=true;
+        }  
+    }, false );
+}
+
+var lineLayer = L.layerGroup().addTo(map);
+
+function drawLines(){
+    var p0, p1;
+    lineLayer.clearLayers();
+    if(markers.length>1){
+        for(var i=1;i<markers.length;i++){
+            p0=markers[i].getLatLng();
+            p1=markers[i-1].getLatLng();
+
+            // create a red polyline from an array of LatLng points
+            var newLine = L.polyline([p0,p1], {color: 'blue'}).addTo(lineLayer);
+        }
+        
+    }
+}
+
